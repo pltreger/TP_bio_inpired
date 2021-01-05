ade604cf92107cb8110a381b2e0ee12698611d9d
pltreger <pltreger@gmail.com>
2021-01-05 20:14:25 +0100

    maj rapport

---
+++ venv2/Lib/site-packages/vizdoomgym/vizdoomgym/envs/__init__.py
@@ -0,0 +1,13 @@
+from vizdoomgym.envs.vizdoomenv import VizdoomEnv
+from vizdoomgym.envs.vizdoom_env_definitions import (
+    VizdoomBasic,
+    VizdoomCorridor,
+    VizdoomDefendCenter,
+    VizdoomDefendLine,
+    VizdoomHealthGathering,
+    VizdoomMyWayHome,
+    VizdoomPredictPosition,
+    VizdoomTakeCover,
+    VizdoomDeathmatch,
+    VizdoomHealthGatheringSupreme,
+)
